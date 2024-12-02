6f1be6983a656b7867c9e9056795804c36016352
Parciamigisha <157398538+Parciamigisha@users.noreply.github.com>
2024-12-02 13:24:20 -0500

    gaining access of the webcam

--- object_oriented.py
+++ object_oriented.py
@@ -3,22 +3,25 @@
 of the virtual game'''
 import cv2
 
+class tunnal:
+	pass
+
 
 class facefinder:
 	'''this is going to use haar cascade as filter to detect the largest face'''
 
-	def __int__(self):
+	def __init__(self):
 		print("face finder initialized")
 		self.face_cascade=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 
-	def find_face(slf, frame):
+	def find_face(self, frame):
 		'''returns face center(x,y), also draws a rect on face'''
-		gray = cv2.cvtColor(gray, cv2.COLOR_BGR2GRAY)
+		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 		faces = self.face_cascade.detectMultiScale(gray, minNeighbors = 9)
 
-		if faces is none:
-			return none
+		if faces is None:
+			return None
 
 		bx = by = bw = bh = 0 
 
@@ -32,4 +35,29 @@ class facefinder:
 '''------------------------------------------------------------'''
 ##main code
 ff = facefinder()
+#get accesse to the web cap
+cap = cv2.VideoCapture(cv2.CAP_ANY)
+if not cap.isOpened():
+	print("couldn't open web cam")
+	exit()
+
+
+while True:
+	retval, frame = cap.read()
+	if retval == False:
+		print("camera error")
+
+	ff.find_face(frame)
+	cv2.imshow('q to quit', frame)
+
+	if cv2.waitKey(30) == ord("q"):
+		break
+
+
+
+pause = input("press enter to end")
+
+#destroys the cam
+cap.release()
+
 print ('starting oo virtual3D')
